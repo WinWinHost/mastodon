b7565f1dddaceb83f67aa6c1c29b9d10baf209ed
Eugen Rochko <eugen@zeonfederated.com>
2018-06-01 04:00:02 +0200

    WIP

--- app/models/trending_tags.rb
+++ app/models/trending_tags.rb
@@ -8,11 +8,11 @@ class TrendingTags
 
   class << self
     def record_use!(tag, account, at_time = Time.now.utc)
-      return if disallowed_hashtags.include?(tag.name) || account.silenced?
+      return if disallowed_hashtags.include?(tag.name) || account.silenced? || account.bot?
 
-      increment_vote!(tag.id, at_time)
       increment_historical_use!(tag.id, at_time)
       increment_unique_use!(tag.id, account.id, at_time)
+      increment_vote!(tag.id, at_time)
     end
 
     def get(limit)
@@ -24,8 +24,16 @@ class TrendingTags
     private
 
     def increment_vote!(tag_id, at_time)
-      redis.zincrby(KEY, (2**((at_time.to_i - epoch) / HALF_LIFE)).to_f, tag_id.to_s)
-      redis.zremrangebyrank(KEY, 0, -MAX_ITEMS) if rand < (2.to_f / MAX_ITEMS)
+      expected = redis.pfcount("activity:tags:#{tag_id}:#{(at_time - 1.day).beginning_of_day.to_i}:accounts")
+      expected = 1 if expected.zero?
+      observed = redis.pfcount("activity:tags:#{tag_id}:#{at_time.beginning_of_day.to_i}:accounts")
+
+      return if expected > observed
+
+      chi_score = ((observed.to_f - expected.to_f) ** 2) / expected.to_f
+
+      redis.zadd(KEY, chi_score, tag_id.to_s)
+      redis.zremrangebyscore(KEY, 0, 0)
     end
 
     def increment_historical_use!(tag_id, at_time)
